417a0ae71a06f48510ccfd2dcfe6a4573082fe57
Wagner Kroiss <110502518+wagnerk78@users.noreply.github.com>
2023-07-17 10:30:15 -0300

    17th July

--- src/main/java/br/com/supera/desafio4/Desafio4.java
+++ src/main/java/br/com/supera/desafio4/Desafio4.java
@@ -3,29 +3,40 @@ package br.com.supera.desafio4;
 import java.util.Scanner;
 
 public class Desafio4 {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Quantas mensagens você quer decifrar ?");
-        int quantidade = Integer.parseInt(scan.next());
-        String teste = null;
-        if (quantidade > 0) {
-            for (int i = 0; i < quantidade; i++) {
-                System.out.println("Digite a " + (i + 1) + "ª" + " mensagem: ");
-                teste = scan.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a quantidade de frases a serem decriptografadas: ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] frases = new String[quantidade];
+
+        for (int i = 0; i < frases.length; i++) {
+            System.out.print("Digite a frase " + (i + 1) + ": ");
+            frases [i] = scanner.nextLine();
+        }
+
+        System.out.println("Frases descriptografadas:");
+
+        for (String string : frases) {
+            //System.out.println(string);
+
+            int n = 0;
+            n = string.length();
+            for (int i = n/2 - 1; i > -1 ; i--) {
+                System.out.print(string.charAt(i));
             }
+            for (int i = n-1; i > n/2 - 1 ; i--) {
+                System.out.print(string.charAt(i));
+        }
+            System.out.print("\n");
+
 
         }
-        int n=0;
-        n = teste.length();
-        for (int i = n/2 - 1; i > -1 ; i--) {
-            System.out.print(teste.charAt(i));
-        }
-        for (int i = n-1; i > n/2 - 1 ; i--) {
-            System.out.print(teste.charAt(i));
-        }
+
+
+
 
 
     }
-    }
-
+}
